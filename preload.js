a79b39d372393091de60d92a8c8d722b827ce9fe
swastikaa303 <swastikabaniya77@gmail.com>
2026-03-26 11:15:08 +0900

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,6 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    saveNote: (text) => ipcRenderer.invoke('save-note', text),
+    loadNote: () => ipcRenderer.invoke('load-note')
+});
